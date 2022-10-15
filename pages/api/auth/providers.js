5776616d6b85e867c700bce3748546c0a861ff14
borispipo <borispipo@gmail.com>
2022-10-15 20:17:51 +0100

    Prise en compte de l'api listant les différents providers de l'application

---
+++ pages/api/auth/providers.js
@@ -0,0 +1,17 @@
+import {providers} from "$nauth";
+import {SUCCESS} from "$capi/status";
+import {isObj} from "$cutils";
+
+export default function handler(req,res){
+    for(let i in providers){
+        const p = providers[i];
+        if(isObj(p)){
+            for(let k in p){
+                if(typeof p[k] =='function'){
+                    delete p[k];
+                }
+            }
+        }
+    }
+    return res.status(SUCCESS).json({data:providers})
+}
