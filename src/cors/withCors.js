dff761c910870f20076eaf7575fdccaa491e875e
borispipo <borispipo@gmail.com>
2022-10-27 10:31:41 +0100

    Update withCors

--- src/cors/withCors.js
+++ src/cors/withCors.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 import cors from "./index";
-export default async function withCors (hanlder){
+export default function withCors (hanlder){
     return async (req,res,event)=>{
         await cors(req,res);
         return await hanlder(req,res,event);
