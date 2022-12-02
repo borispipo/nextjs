2469ad272ee1e9f6a767a7adcfeca8a30f15f3cd
borispipo <borispipo@gmail.com>
2022-12-02 07:02:47 +0100

    remplacer le fichier bcrypt par crypto pour la cryptage des mots de pass

--- src/auth/utils/password.js
+++ src/auth/utils/password.js
@@ -3,63 +3,54 @@
 // license that can be found in the LICENSE file.
 import {NOT_FOUND,UNAUTHORIZED} from "$api";
 import isNonNullString from "$cutils/isNonNullString";
+import { randomBytes, scryptSync } from 'crypto';
 
 const bcrypt = require('bcrypt');
+//@see : https://stackoverflow.com/questions/19822643/what-is-an-alternative-for-bcrypt-to-use-with-node
 
 /**** crypter le mot de passe passé en paramètre 
  * @param {string} password le mot de passe à crypter
  * @param {function} callback la fonction de rappel à utiliser lorsque le mot de passe est crypté
 */
-export const encryptPassword = function(password, callback) {
-    if(!isNonNullString(password)) return Promise.reject({status:UNAUTHORIZED,message:'Vous devez spécifier un mot de pass non null'});
-    return new Promise((resolve,reject)=>{
-        bcrypt.genSalt(10, function(err, salt) {
-            if (err) {
-                if(typeof callback =='function'){
-                    callback(err);
-                }
-                return reject(err);
-            }
-            bcrypt.hash(password, salt, function(err, hash) {
-                if(typeof callback =='function'){
-                    callback(err, hash)
-                }
-                if(err){
-                    return reject(err);
-                }
-                return resolve(hash);
-            });
-        });
-    })
- };
- /**** compare le mot de passe utilisateur passé en paramètre
-  * @param {string} plainPass le mot de passe bruite passé en paramètre
-  * @param {string} hashword le mot de passe crypté enregistré en base de données
-  * @param {function} callback la fonction de rappel 
-  */
- export const comparePassword = function(plainPass, hashword, callback) {
-    if(!isNonNullString(plainPass)){
-        return Promise.reject({status:UNAUTHORIZED,message : 'Vous devez spécifier un mot de pass non null'});
+// Pass the password string and get hashed password back
+// ( and store only the hashed string in your database)
+export const encryptPassword = (password,salt) => {
+    if(!isNonNullString(password)) {
+        throw {status:UNAUTHORIZED,message:'Vous devez spécifier un mot de pass non null'};
     }
-    if(!isNonNullString(hashword)){
-        return Promise.reject({status:UNAUTHORIZED,message : 'Vous devez spécifier un mot de pass de comparaison non null'});
+    if(!isNonNullString(salt)){
+        salt = randomBytes(16).toString('hex');
     }
-    return new Promise((resolve,reject)=>{
-        bcrypt.compare(plainPass, hashword, function(err, isPasswordMatch) {   
-            if(typeof callback =='function'){
-                callback(err, err ? false : isPasswordMatch);
-            }
-            if(err){
-                return reject(err);
-            }
-            if(!isPasswordMatch){
-                const error = {status:UNAUTHORIZED,message:'Mot de pass incorrect'};
-                if(typeof callback =='function'){
-                    callback(error);
-                }
-                return reject(error);
-            }
-            resolve(true);
-        });
-    })
- };
+    return scryptSync(password, salt, 32).toString('hex');
+};
+/**
+ * Hash password with random salt
+ * @return {string} password hash followed by salt
+ *  XXXX till 64 XXXX till 32
+ *  
+ */
+ export const hashPassword = (password) => {
+    // Any random string here (ideally should be at least 16 bytes)
+    const salt = randomBytes(16).toString('hex');
+    return encryptPassword(password, salt) + salt;
+};
+
+/**
+ * compare le mot de passe utilisateur passé en paramètre
+ *  @param {string} password le mot de passe bruite passé en paramètre
+  * @param {string} hash le mot de passe crypté enregistré en base de données
+ */
+ export const comparePassword = (password, hash) => {
+    if(!isNonNullString(password)){
+        throw ({status:UNAUTHORIZED,message : 'Vous devez spécifier un mot de pass non null'});
+    }
+    if(!isNonNullString(hash)){
+        throw ({status:UNAUTHORIZED,message : 'Vous devez spécifier un mot de pass de comparaison non null'});
+    }
+    // extract salt from the hashed string
+    // our hex password length is 32*2 = 64
+    const salt = hash.slice(64);
+    const originalPassHash = hash.slice(0, 64);
+    const currentPassHash = encryptPassword(password, salt);
+    return originalPassHash === currentPassHash;
+};
