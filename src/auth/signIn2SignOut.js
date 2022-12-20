02bbaaa74b8f99ca094bad249365942e5d41031d
borispipo <borispipo@gmail.com>
2022-12-20 18:41:40 +0100

    Export de la fonction isMasterAdmin dans $nauthSignIn2SignOut

--- src/auth/signIn2SignOut.js
+++ src/auth/signIn2SignOut.js
@@ -3,13 +3,15 @@
 // license that can be found in the LICENSE file.
 import {isObj} from "$utils";
 import { getProvider } from "./providers";
+
+export const isMasterAdmin = (user)=>{
+    if(!isObj(user)) return false;
+    const provider = getProvider(user.providerId);
+    if(!isObj(provider)) return false;
+    if(typeof provider.isMasterAdmin =="function") return provider.isMasterAdmin(user);
+    return user.isMasterAdmin ? true : false;
+}
 export default {
     /**** l'utilisateur connecté avec le compte client est le super user de l'application */
-    isMasterAdmin : (user)=>{
-        if(!isObj(user)) return false;
-        const provider = getProvider(user.providerId);
-        if(!isObj(provider)) return false;
-        if(typeof provider.isMasterAdmin =="function") return provider.isMasterAdmin(user);
-        return user.isMasterAdmin ? true : false;
-    },
+    isMasterAdmin,
 }
