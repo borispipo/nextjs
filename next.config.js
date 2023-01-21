c0f696fb9c6ffaab1116c61c72854536ff5b108c
borispipo <borispipo@gmail.com>
2023-01-21 11:04:46 +0100

    Prise en compte de la fonction unfetch en environnement nodejs

--- next.config.js
+++ next.config.js
@@ -113,6 +113,9 @@ module.exports = (opts)=>{
     },
     webpack: (config,options) => {
       const { isServer, buildId, dev, defaultLoaders, nextRuntime, webpack } = options;
+      if(isServer){
+        alias.$unfetch = path.resolve(src,"api","unfetch");
+      }
       config.resolve.alias = {
         ...(config.resolve.alias || {}),
         ...alias,
---
+++ src/api/unfetch.js
@@ -0,0 +1,21 @@
+import fetch, {
+    Blob,
+    blobFrom,
+    blobFromSync,
+    File,
+    fileFrom,
+    fileFromSync,
+    FormData,
+    Headers,
+    Request,
+    Response,
+  } from 'node-fetch'
+  
+  if (!globalThis.fetch) {
+    globalThis.fetch = fetch
+    globalThis.Headers = Headers
+    globalThis.Request = Request
+    globalThis.Response = Response
+  }
+  
+  export default fetch;
