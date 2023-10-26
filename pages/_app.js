828d0476ca5ea1a54d352afde5bc71b784295167
borispipo <borispipo@gmail.com>
2023-10-26 20:26:06 +0100

    Update du provider, pour fix bug lié au build de l'application

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,9 @@
 const appConfig = require("$capp/config");
-import {AuthProvider} from "$cauth";
-import {isObj} from "$utils";
-import { getProvider } from "../src/auth/providers";
-
-export const isMasterAdmin = (user)=>{
-    if(!isObj(user)) return false;
-    const provider = getProvider(user.providerId);
-    if(!isObj(provider)) return false;
-    if(typeof provider.isMasterAdmin =="function") return provider.isMasterAdmin(user);
-    return user.isMasterAdmin ? true : false;
-}
 
 function MyApp({ Component,config, pageProps }) {
   config = typeof config =='object' && config ? config : {};
   appConfig.current = config;
-  return <AuthProvider isMasterAdmin={isMasterAdmin}>
-    <Component {...pageProps} />
-  </AuthProvider>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
--- src/auth/index.js
+++ src/auth/index.js
@@ -6,6 +6,10 @@
  * @module $next-js/auth- exporte toutes les fonctions utilles de @nextjs
  * @see @module $cauth
  */
+import {isObj,extendObj} from "$utils";
+import { getProvider } from "./providers";
+import { signInRef } from "$cauth/authSignIn2SignOut";
+
 export * from "./utils";
 
 export {default as providers} from "./providers";
@@ -15,3 +19,13 @@ export * from "./providers";
 export * from "$auth-utils";
 
 export * from "./utils/password";
+
+extendObj(signInRef.current,{
+    isMasterAdmin : (user)=>{
+        if(!isObj(user)) return false;
+        const provider = getProvider(user.providerId);
+        if(!isObj(provider)) return false;
+        if(typeof provider.isMasterAdmin =="function") return provider.isMasterAdmin(user);
+        return user.isMasterAdmin ? true : false;
+    },
+});
