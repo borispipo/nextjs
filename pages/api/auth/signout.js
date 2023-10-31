c58d2f1f4aa5479a2f032061e2fce5c22115019d
borispipo <borispipo@gmail.com>
2023-10-31 19:41:16 +0100

    Fix bug re is not defined FIX SESSION ON LOGOUT

--- pages/api/auth/signout.js
+++ pages/api/auth/signout.js
@@ -2,6 +2,7 @@ import { removeTokenCookie } from '$nauth';
 import { SUCCESS } from '$capi/status';
 import withCors from "$withCors";
 import {AUTH} from "$nevents";
+import { getProviderSession } from '$nauth/utils/auth-cookies';
 /****
  * @api {get} /auth/logout Déconnecter un utilisateur
  * @apiName {Logout User}
@@ -10,7 +11,7 @@ import {AUTH} from "$nevents";
  * 
  */
 export default withCors(async function logout(req, res) {
-  const session = re.session;
+  const session = await getProviderSession(req);
   removeTokenCookie(res)
   AUTH.emit("signout",session);
   res.status(SUCCESS).json({message:'Vous vous êtes déconnecté avec succès!!'})
